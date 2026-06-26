65366d7827719aed11fefba107570e9469c22329
prasadkothakotla <kottakotladeviprasad@gmail.com>
2026-06-26 21:59:30 +0530

    Initial commit

--- internshala_scrapper.js
+++ internshala_scrapper.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Server running');
+});
 app.post('/scrape', async (req,res)=>{
 
     const role = req.body.role;
