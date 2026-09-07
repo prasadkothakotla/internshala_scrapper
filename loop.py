875c4d286bc87b9f4218946c98d9547559a1c3d9
Devi Prasad Kothakotla <kottakotladeviprasad@gmail.com>
2026-09-07 05:34:55 +0530

    Implement discount calculation function
    Added a function to calculate discounts on prices.

--- loop.py
+++ loop.py
@@ -9,3 +9,15 @@ def find_common_items(list1, list2):
     return result
 n=10
 print(n/0)
+
+def calculate_discount(price, discount):
+    if discount < 0 or discount > 100:
+        raise ValueError("Invalid discount")
+
+    return price * (1 - discount / 100)
+
+
+price = 100
+discount = 20
+
+print(calculate_discount(price, discount))
